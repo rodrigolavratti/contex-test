608d95d1636a445c1632c8e981b710fe519e3f99
rodrigo.lavratti <rodrigo.lavratti@compasso.com.br>
2022-09-02 22:54:57 -0300

    React Context

--- src/components/CustomerForm.jsx
+++ src/components/CustomerForm.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
-const CustomerForm = () => {
+const CustomerForm = ({onSubmit}) => {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("dados", {name, email})
+
+        onSubmit({name, email})
+    }
+
     return (
-        <p>CustomerForm</p>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Nome:</label>
+            <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" valeu={email} onChange={(e) => setEmail(e.target.value)} />
+
+            <button type="Submit">Enviar</button>
+        </form>
     )
 }
 export default CustomerForm
---
+++ src/components/Hello.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+const Hello = ({name}) => {
+    return <h1>{name}</h1>
+}
+export default Hello
